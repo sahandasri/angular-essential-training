9c25c6b8340100d85d90202435fb5926d485868c
Sahand Asri <sahandasri@gmail.com>
2021-07-26 16:04:31 -0400

    add styleUrl

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,8 @@ import {Component} from '@angular/core';
 
 @Component({
   selector: 'mw-app',
-  templateUrl: 'app.component.html'
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.css']
 })
 
 export class AppComponent {}
