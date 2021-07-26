36f61d9ce8dbc7f66390dfcac0d41b202ce41f7a
Sahand Asri <sahandasri@gmail.com>
2021-07-26 15:54:08 -0400

    add templateUrl

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,8 @@
 import {Component} from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  template: '<h1>Hello World</h1>'
+  selector: 'mw-app',
+  templateUrl: 'app.component.html'
 })
 
 export class AppComponent {}
